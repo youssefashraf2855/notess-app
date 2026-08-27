bc28173d5c86ce10bc1a76e7e25935986822854b
Your Name <youssefashraf2855@gmail.com>
2026-08-27 16:55:52 +0300

    styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     Hello World
+     <div>
+      <form action="" method="post">
+      <h2>Title</h2>
+      <input type="text" />
+      <h2>Content</h2>
+      <input type="text" />
+      </form>
+     </div>
     </div>
   );
 }
